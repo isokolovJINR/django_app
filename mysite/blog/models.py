5f4d3837a6fa24a74cc0eb320e11724639d627d8
isokolov <isokolov@jinr.ru>
2022-10-17 19:28:22 +0300

    add tags and forms comments

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -4,6 +4,7 @@ from django.urls import reverse
 from django.utils import timezone
 
 # Create your models here.
+from taggit.managers import TaggableManager
 
 
 class PublishedManager(models.Manager):
@@ -14,6 +15,7 @@ class PublishedManager(models.Manager):
 
 
 class Post (models.Model):
+    tags = TaggableManager()
     objects = models.Manager()
     published = PublishedManager()
     STATUS_CHOICES = (
@@ -45,3 +47,19 @@ class Post (models.Model):
                              self.publish.month,
                              self.publish.day, self.slug])
 
+
+class Comments(models.Model):
+    post = models.ForeignKey(Post,
+                             on_delete=models.CASCADE,
+                             related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('created',)
+    def __str__(self):
+        return f'Comment by {self.name} on {self.post}'
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog.apps.BlogConfig'
+    'blog.apps.BlogConfig',
+    'taggit'
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
