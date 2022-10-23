eae1dd1ad2410b89b41925d13519469c35ade020
isokolov <isokolov@jinr.ru>
2022-10-23 18:13:38 +0300

    Add login page

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request,
+                                username=cd['username'],
+                                password=cd['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse('Authenticated '
+                                        'successfully')
+                else:
+                    return HttpResponse('DIS akk')
+            else:
+                return HttpResponse('Invalide')
+    else:
+        form = LoginForm()
+    return  render(request, 'account/login.html', {'form': form})
+
