7deef3e4995feb60b273476a294db7126f0d15d0
isokolov <redfenix15@gmail.com>
2022-11-03 16:05:18 +0300

    add celery

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -130,3 +130,4 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 CART_SESSION_ID = 'cart'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
