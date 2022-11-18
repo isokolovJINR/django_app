a7a1d4f7995053c3b04531d7af8d058695796bfa
isokolov <redfenix15@gmail.com>
2022-11-18 11:30:17 +0300

    add mixin, courses and templates

--- educa/courses/views.py
+++ educa/courses/views.py
@@ -4,6 +4,8 @@ from .models import Course
 from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, \
+    PermissionRequiredMixin
 # Create your views here.
 
 
@@ -19,7 +21,9 @@ class OwnerEditMixin(object):
         return super().form_valid(form)
 
 
-class OwnerCourseMixin(object):
+class OwnerCourseMixin(OwnerMixin,
+                       LoginRequiredMixin,
+                       PermissionRequiredMixin):
     model = Course
     fields = ['subject', 'title', 'slug', 'overview']
     success_url = reverse_lazy('manage_course_list')
@@ -31,12 +35,21 @@ class OwnerCourseEditMixin(OwnerCourseMixin, OwnerEditMixin):
 
 class ManageCourseListView(OwnerCourseMixin, OwnerMixin):
     template_name = 'courses/manage/course/list.html'
+    permission_required = 'courses.view_course'
+
+
 class CourseCreateView(OwnerCourseEditMixin, CreateView):
-    pass
+    permission_required = 'courses.add_course'
+
+
 class CourseUpdateView(OwnerCourseEditMixin, UpdateView):
-    pass
+    permission_required = 'courses.change_course'
+
+
 class CourseDeleteView(OwnerCourseMixin, DeleteView):
     template_name = 'courses/manage/course/delete.html'
+    permission_required = 'courses.delete_course'
+
 
 class ManageCourseListView(ListView):
     model = Course
