c32cc71b4e83e7842152e7c259b11cdd12be1f88
isokolov <redfenix15@gmail.com>
2022-11-24 16:44:44 +0300

    add mixin, courses and templates

--- educa/educa/settings.py
+++ educa/educa/settings.py
@@ -31,13 +31,17 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+
     'courses.apps.CoursesConfig',
+    'guardian',
+    'groups_manager',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
 ]
 
 MIDDLEWARE = [
